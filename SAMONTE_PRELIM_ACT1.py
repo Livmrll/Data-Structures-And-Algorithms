509c27ff9fe0873bb3d85227e3a467634f61f342
Liv <rianamliv9666@gmail.com>
2026-02-02 15:23:50 +0800

    Add files via upload

---
+++ SAMONTE_PRELIM_ACT1.py
@@ -0,0 +1,59 @@
+Ten_Car_Brands = ["Ferrari","Lamborghini","Bugatti","Porsche","Ford","Toyota","Pagani","Maserati","McLaren","Koegnigsegg"]
+Extension_Car_Brands = ["Hyundai","Mitsubishi","Isuzu","Honda"]
+print("The 1st Four(4) Elements:", Ten_Car_Brands[:4])
+print("Extend Four(4) More Elements:", Extension_Car_Brands)
+print("Original:", Ten_Car_Brands)
+Ten_Car_Brands.extend(Extension_Car_Brands)
+print("Updated:", Ten_Car_Brands)
+print("The Last Three(3) Elements:", Ten_Car_Brands[-3:])
+Middle_Car_Brands = len(Ten_Car_Brands) // 2
+print("The Middle Elements:", Ten_Car_Brands[Middle_Car_Brands-1:Middle_Car_Brands+1])
+print("\n")
+
+Ten_Restaurants_Name = ["Jollibee","McDonalds","KFC","ArmyNavy","Mang Inasal","Chowking","Greenwich","Popeyes","Burger King","Shakey's Pizza"]
+Extension_Restaurants_Name = ["Mesa","KuyaJ","Manang","Peri-Peri"]
+print("The 1st Four(4) Elements:", Ten_Restaurants_Name[:4])
+print("Extended Four(4) More Elements:",Extension_Restaurants_Name)
+print("Original:", Ten_Restaurants_Name)
+Ten_Restaurants_Name.extend(Extension_Restaurants_Name)
+print("Updated:", Ten_Restaurants_Name)
+print("The Last Three(3) Elements:", Ten_Restaurants_Name[-3:])
+Middle_Restaurants_Name = len(Ten_Restaurants_Name) // 2
+print("The Middle Elements:", Ten_Restaurants_Name[Middle_Restaurants_Name-1:Middle_Restaurants_Name+1])
+print("\n")
+
+Ten_Customer_Name = ["Nini","Niel","Jhamir","Tom","Danica","Cedric","Dirk","Jonard","Arman","Kurt"]
+Extension_Customer_Name = ["Trob","Harry","Clyde","Madison"]
+print("The 1st Four(4) Elements:", Ten_Customer_Name[:4])
+print("Extended Four(4) More Elements:",Extension_Customer_Name)
+print("Original:", Ten_Customer_Name)
+Ten_Customer_Name.extend(Extension_Customer_Name)
+print("Updated:", Ten_Customer_Name)
+print("The Last Three(3) Elements:", Ten_Customer_Name[-3:])
+Middle_Customer_Name = len(Ten_Customer_Name) // 2
+print("The Middle Elements:", Ten_Customer_Name[Middle_Customer_Name-1:Middle_Customer_Name+1])
+print("\n")
+
+Ten_City_In_The_NCR = ["Quezon City","Manila","Caloocan","Taguig","Pasig","Valenzuela","Makati","Parañaque","Las Piñas","Muntinlupa"]
+Extension_City_In_The_NCR = [""]
+print("\n")
+
+Ten_Hotels = ["The Manila Hotel","Okada Manila","The Peninsula","Shangri-La The Fort","Fili Hotel","Solaire Resort","Conrad Manila","Fairmont Makati","Atmosphere Resort & Spa","Discovery Boracay"]
+Extension_Hotels = [""]
+print("\n")
+
+Ten_Universities = ["LPU","UP","ADMU","DLSU","UST","PUP","FEU","USC","EU","NU"]
+Extension_Universities = [""]
+print("\n")
+
+Ten_Provinces = ["Cebu City","Cavite","Bulacan","Laguna","Pangasinan","Batangas","Palawan","Pampanga","IloIlo City","Rizal"]
+Extension_Provinces = [""]
+print("\n")
+
+Ten_Computer_Brands = ["ASUS","Acer","Lenovo","Apple","Dell","MSI","Huawei","ROG","Samsung","HP"]
+Extension_Computer_Brands = [""]
+print("\n")
+
+Ten_Shoe_Brands = ["Nike","Adidas","New Balance","Skechers","World Balance","Vans","Converse","Crocs","Gibi Shoes","Birkenstock"]
+Extension_Shoe_Brands = [""]
+print("\n")
