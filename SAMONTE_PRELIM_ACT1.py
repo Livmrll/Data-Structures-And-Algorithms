a1bfea7e715818d1d0ac6b610fc548cb206bcf4d
Liv <rianamliv9666@gmail.com>
2026-02-03 08:26:42 +0800

    Add extensions to various lists in SAMONTE_PRELIM_ACT1

--- SAMONTE_PRELIM_ACT1.py
+++ SAMONTE_PRELIM_ACT1.py
@@ -35,25 +35,72 @@ print("The Middle Elements:", Ten_Customer_Name[Middle_Customer_Name-1:Middle_Cu
 print("\n")
 
 Ten_City_In_The_NCR = ["Quezon City","Manila","Caloocan","Taguig","Pasig","Valenzuela","Makati","Parañaque","Las Piñas","Muntinlupa"]
-Extension_City_In_The_NCR = [""]
+Extension_City_In_The_NCR = ["Marikina","Navotas","Mandaluyong","San Juan"]
+print("The 1st Four(4) Elements:", Ten_City_In_The_NCR[:4])
+print("Extended Four(4) More Elements:", Extension_City_In_The_NCR)
+print("Original:", Ten_City_In_The_NCR)
+Ten_City_In_The_NCR.extend(Extension_City_In_The_NCR)
+print("Updated:", Ten_City_In_The_NCR)
+print("The Last Three(3) Elements:", Ten_City_In_The_NCR[-3:])
+Middle_City_In_The_NCR = len(Ten_City_In_The_NCR) // 2
+print("The Middle Elements:", Ten_City_In_The_NCR[Middle_City_In_The_NCR-1:Middle_City_In_The_NCR+1])
 print("\n")
 
 Ten_Hotels = ["The Manila Hotel","Okada Manila","The Peninsula","Shangri-La The Fort","Fili Hotel","Solaire Resort","Conrad Manila","Fairmont Makati","Atmosphere Resort & Spa","Discovery Boracay"]
-Extension_Hotels = [""]
+Extension_Hotels = ["The Ritz-Carlton","Mandarin Oriental","InterContinental Manila","The St. Regis Manila","Hilton Manila"]
+print("The 1st Four(4) Elements:", Ten_Hotels[:4])
+print("Extended Four(4) More Elements:", Extension_Hotels)
+print("Original:", Ten_Hotels)
+Ten_Hotels.extend(Extension_Hotels)
+print("Updated:", Ten_Hotels)
+print("The Last Three(3) Elements:", Ten_Hotels[-3:])
+Middle_Hotels = len(Ten_Hotels) // 2
+print("The Middle Elements:", Ten_Hotels[Middle_Hotels-1:Middle_Hotels+1])
 print("\n")
 
 Ten_Universities = ["LPU","UP","ADMU","DLSU","UST","PUP","FEU","USC","EU","NU"]
-Extension_Universities = [""]
+Extension_Universities = ["MAPUA","TIP","SBU","EAC"]
+print("The 1st Four(4) Elements:", Ten_Universities[:4])
+print("Extended Four(4) More Elements:", Extension_Universities)
+print("Original:", Ten_Universities)
+Ten_Universities.extend(Extension_Universities)
+print("Updated:", Ten_Universities)
+print("The Last Three(3) Elements:", Ten_Universities[-3:])
+Middle_Universities = len(Ten_Universities) // 2
+print("The Middle Elements:", Ten_Universities[Middle_Universities-1:Middle_Universities+1])
 print("\n")
 
 Ten_Provinces = ["Cebu City","Cavite","Bulacan","Laguna","Pangasinan","Batangas","Palawan","Pampanga","IloIlo City","Rizal"]
-Extension_Provinces = [""]
+Extension_Provinces = ["Zambales","Bataan","Nueva Ecija","Benguet"]
+print("The 1st Four(4) Elements:", Ten_Provinces[:4])
+print("Extended Four(4) More Elements:", Extension_Provinces)
+print("Original:", Ten_Provinces)
+Ten_Provinces.extend(Extension_Provinces)
+print("Updated:", Ten_Provinces)
+print("The Last Three(3) Elements:", Ten_Provinces[-3:])
+Middle_Provinces = len(Ten_Provinces) // 2
+print("The Middle Elements:", Ten_Provinces[Middle_Provinces-1:Middle_Provinces+1])
 print("\n")
 
 Ten_Computer_Brands = ["ASUS","Acer","Lenovo","Apple","Dell","MSI","Huawei","ROG","Samsung","HP"]
-Extension_Computer_Brands = [""]
+Extension_Computer_Brands = ["Razer","Logitech", "Corsair", "Gigabyte"]
+print("The 1st Four(4) Elements:", Ten_Computer_Brands[:4])
+print("Extended Four(4) More Elements:", Extension_Computer_Brands)
+print("Original:", Ten_Computer_Brands)
+Ten_Computer_Brands.extend(Extension_Computer_Brands)
+print("Updated:", Ten_Computer_Brands)
+print("The Last Three(3) Elements:", Ten_Computer_Brands[-3:])
+Middle_Computers_Brands = len(Ten_Computer_Brands) // 2
+print("The Middle Elements:", Ten_Computer_Brands[Middle_Computers_Brands-1:Middle_Computers_Brands+1])
 print("\n")
 
 Ten_Shoe_Brands = ["Nike","Adidas","New Balance","Skechers","World Balance","Vans","Converse","Crocs","Gibi Shoes","Birkenstock"]
-Extension_Shoe_Brands = [""]
-print("\n")
+Extension_Shoe_Brands = ["Puma","Reebok","Under Armour","Fila"]
+print("The 1st Four(4) Elements:", Ten_Shoe_Brands[:4])
+print("Extended Four(4) More Elements:", Extension_Shoe_Brands)
+print("Original:", Ten_Shoe_Brands)
+Ten_Shoe_Brands.extend(Extension_Shoe_Brands)
+print("Updated:", Ten_Shoe_Brands)
+print("The Last Three(3) Elements:", Ten_Shoe_Brands[-3:])
+Middle_Shoes_Brands = len(Ten_Shoe_Brands) // 2
+print("The Middle Elements:", Ten_Shoe_Brands[Middle_Shoes_Brands-1:Middle_Shoes_Brands+1])
